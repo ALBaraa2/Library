6d58c03821ce19db503420bfd3bccddc029f7bf5
ALBaraa <albaraawork3@gmail.com>
2025-06-03 13:48:25 +0300

    Middleware for routes

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,23 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return redirect()->route('books.index');
 });
 
-Route::resource('books', BookController::class);
-Route::resource('authors', AuthorController::class);
-Route::resource('books.reviews', ReviewController::class)
-    ->scoped(['review' => 'id'])
-    ->only(['create', 'store', 'destroy']);
+Route::get('books', [BookController::class, 'index'])->name('books.index');
+Route::middleware('auth')->group(function () {
+    Route::resource('books', BookController::class)->except('index');
+    Route::resource('authors', AuthorController::class);
+    Route::resource('books.reviews', ReviewController::class)
+        ->scoped(['review' => 'id'])
+        ->only(['create', 'store', 'destroy']);
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+});
 
 
-Route::get('login', [AuthController::class, 'showLogin'])->name('show.login');
-Route::get('register', [AuthController::class, 'showRegister'])->name('show.register');
-Route::post('login', [AuthController::class, 'login'])->name('login');
-Route::post('register', [AuthController::class, 'register'])->name('register');
-Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware('guest')->controller(AuthController::class)->group(function () {
+    Route::get('login', [AuthController::class, 'showLogin'])->name('show.login');
+    Route::get('register', [AuthController::class, 'showRegister'])->name('show.register');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+});
